1a0582ba1640e61e5e0f90c9baf6f855204eb602
the company <thecompany001@protonmail.com>
2021-12-28 19:05:08 -0500

    2sum unsorted array

--- 2_sum_sorted_array.py
+++ 2_sum_sorted_array.py
@@ -4,18 +4,16 @@ def pair_sum_sorted(numbers, target):
     
     while left_point < right_point:
         current_sum = numbers[left_point] + numbers[right_point]
-        if current_sum > target:
-            right_point -= 1
+        if current_sum == target:
+            return ([left_point, right_point])
         elif current_sum < target:
             left_point += 1
         else:
-            current_sum == target
-        return [left_point, right_point]
-    return [-1, -1]
+            right_point -= 1
+    return ([-1, -1])
 
 def test():
-    result = pair_sum_sorted([1, 2, 3, 5, 10], 15)
-    print("Two Sum with target of 7 is: " + str(result))
+    result = pair_sum_sorted([1, 2, 3, 5, 10], 10)
+    print("Two Sum target is: " + str(result))
     
 test()
-
---
+++ 2_sum_unsorted_array.py
@@ -0,0 +1,15 @@
+def two_sum(nums, target):
+    hashmap = {}
+    
+    for i in range(len(nums)):
+        if target-nums[i] in hashmap:
+            return [i, hashmap[target-nums[i]]]
+        else:
+            hashmap[nums[i]] = i
+    return [-1, -1]
+        
+def test():
+    result = two_sum([5, 3, 10, 45, 1], 8)
+    print("Two Sum target is: " + str(result))
+    
+test()
