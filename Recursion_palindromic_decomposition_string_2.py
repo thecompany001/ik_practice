22c19d56236d3c2687e588cc6d17d5d074232d3d
the company <thecompany001@protonmail.com>
2022-02-10 19:47:56 -0500

    ik prac

--- Recursion_palindromic_decomposition_string_2.py
+++ Recursion_palindromic_decomposition_string_2.py
@@ -141,3 +141,73 @@ def _is_palindrome(string, start, end):
         return True
 
     return string[start] == string[end] and _is_palindrome(string, start+1, end-1)
+
+
+
+XXXXXXX
+
+
+
+from functools import lru_cache
+
+def generate_palindromic_decompositions(s):
+    if not s:
+        return []
+    results = palinRecurse(s)
+    return results
+
+def isPalin(string):
+    return string == string[::-1]
+
+@lru_cache(maxsize = None)
+def palinRecurse(s):
+    s_len = len(s)
+
+    if s_len==1:
+        return [s]
+
+    results = []
+    for i in range(s_len):
+        sub_string = s[:i+1]
+        if isPalin(sub_string):
+            if (i == s_len - 1):
+                results.append(sub_string)
+            else:
+                valid_palins = palinRecurse(s[i+1:])
+                for each_palin in valid_palins:
+                    results.append(sub_string + '|' + each_palin)
+
+    return results
+
+
+XXXXX
+
+
+def generate_palindromic_decompositions(s):
+
+    def is_palindrome(start, end):
+        if s[start] != s[end]:
+            return False
+        elif start >= end:
+            return True
+        else:
+
+            return is_palindrome(start+1, end-1)
+
+    results = {}
+    for start in range(len(s), -1, -1):
+        inter = []
+        for i in range(start, len(s)):
+
+            if is_palindrome(start, i):
+
+                if i == len(s) - 1:
+                    inter.append(s[start:i+1])
+                else:
+
+                    prefix = s[start:i+1]
+                    for suffix in results[i+1]:
+                        tmp = prefix + "|" + suffix
+                        inter.append(tmp)
+                results[start] = inter
+    return results[0]
